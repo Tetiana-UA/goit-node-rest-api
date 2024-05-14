5b05082918119718bb1bb99cf7e68eb57ca3f75d
Tetiana-UA <izidaxm85@gmail.com>
2024-05-14 11:40:36 +0200

    7

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
+import "dotenv/config";
+
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
 
 import contactsRouter from "./routes/contactsRouter.js";
 
-const app = express();
+import "./server.js";
 
+const app = express();
 
 app.use(morgan("tiny"));
 app.use(cors());
